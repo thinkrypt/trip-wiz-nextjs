2f405a37896476e261211f18390d16ed7996c9e3
Thinkrypt <thinkryptic@gmail.com>
2025-11-06 23:11:14 +0530

    feat: add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { ThemeProvider } from '@/components/ThemeProvider';
 import { QueryProvider } from '@/components/QueryProvider';
 
@@ -38,6 +39,7 @@ export default function RootLayout({
 					>
 						{children}
 						<Analytics />
+						<SpeedInsights />
 					</ThemeProvider>
 				</QueryProvider>
 			</body>
